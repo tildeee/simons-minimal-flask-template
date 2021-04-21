f15c20981dc0b9e771450905ee4046eb4b96b49a
simon <simon@adadevelopersacademy.org>
2021-04-20 18:14:05 -0700

    fills out create_app and updates readme with assumptions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # simons-minimal-flask-template
+
+Key assumptions that this scaffolding makes:
+
+- There are two environment variables (accessed in `app/__init__.py`):
+    - `SQLALCHEMY_DATABASE_URI`, which holds the connection string for a development database
+    - `SQLALCHEMY_TEST_DATABASE_URI`, which holds the connection string for a test database
+
+If a Heroku Procfile existed, with this structure, the best contents would be:
+
+```
+web: gunicorn 'app:create_app()'
+```
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,38 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    # Import models here for Alembic setup
+    # Example:
+    # from app.models.model import Model
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+    # Example:
+    # app.register_blueprint(task_page)
+
 
     return app
